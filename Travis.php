65eb1ab1cfaa6b56485975026a9b5d6d58537d66
Tim (robske_110) <robske110@users.noreply.github.com>
2018-08-17 22:13:17 +0200

    Attempt to get travis working again here

--- Travis.php
+++ Travis.php
@@ -1,5 +1,5 @@
 <?php
-$server = proc_open(PHP_BINARY . " src/pocketmine/PocketMine.php --no-wizard --disable-readline", [
+$server = proc_open(PHP_BINARY . " src/pocketmine/PocketMine.php --no-wizard --disable-readline --settings.enable-dev-builds=true", [
 	0 => ["pipe", "r"],
 	1 => ["pipe", "w"],
 	2 => ["pipe", "w"]
@@ -17,7 +17,7 @@ fclose($pipes[2]);
 
 echo "\n\nReturn value: ". proc_close($server) ."\n";
 
-if(count(glob("plugins/DevTools/BanWarn*.phar")) === 0){
+if(count(glob("plugin_data/DevTools/BanWarn*.phar")) === 0){
 	echo "Failed to create BanWarn phar!\n";
 	exit(1);
 }else{
